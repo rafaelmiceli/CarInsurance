67016c6654150159bad5899acb2b6517100dc405
Rafael <rmiceli@stechs.com.ar>
2020-10-12 23:24:50 -0300

    refactor calculatePrice, to avoid multiple ifs in superFullCoverage Product

--- src/Products/SpecialFullCoverage.js
+++ src/Products/SpecialFullCoverage.js
@@ -3,22 +3,28 @@ const Product = require('../Products/Product');
 class SpecialFullCoverage extends Product {
 
     constructor(pros) {
-        super(pros.name, pros.sellIn, pros.price)
+        super(pros.name, pros.sellIn, pros.price);
+
+        this.ranges = [
+            { 'min': -Number.MAX_SAFE_INTEGER, 'max': 0, 'action': 'this.price = 0' },
+            { 'min': 1, 'max': 5, 'action': 'this.alterPrice(3)' },
+            { 'min': 6, 'max': 10, 'action': 'this.alterPrice(2)' },
+            { 'min': 11, 'max': Number.MAX_SAFE_INTEGER, 'action': 'this.alterPrice(1)' },
+        ];
     }
 
+    calculatePrice(sellInValue) {
+        this.ranges.forEach(range => {
+            if (range.min <= sellInValue && sellInValue <= range.max) {
+                eval(range.action);
+                return;
+            }
+        });
+    }
+    
     update() {
         this.alterSellIn(-1);
-
-        if (this.sellIn <= 0) {
-            this.price = 0;
-        } else if (this.sellIn < 5){
-            this.alterPrice(3);
-        } else if (this.sellIn < 10) {
-            this.alterPrice(2);
-        } else {
-            this.alterPrice(1);
-        }
-
+        this.calculatePrice(this.sellIn);
         this.checkMaxPrice();
     }
 }
