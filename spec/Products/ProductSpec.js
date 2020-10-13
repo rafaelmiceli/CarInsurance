18d797028cc849450f1f5d77744beb5ab37b2f1e
Rafael <rmiceli@stechs.com.ar>
2020-10-12 22:32:33 -0300

    constructors fixed

--- spec/Products/ProductSpec.js
+++ spec/Products/ProductSpec.js
@@ -5,7 +5,7 @@ const Product = require('../../src/Products/Product');
 describe("Product", function() {
 
   it("should create Product and set its properties", function() {
-    const product = new Product({ name: 'Product Name', sellIn: 2, price: 10 });
+    const product = new Product('Product Name', 2, 10);
     expect(product.name).equal('Product Name');
     expect(product.sellIn).equal(2);
     expect(product.price).equal(10);
--- src/Products/MegaCoverage.js
+++ src/Products/MegaCoverage.js
@@ -2,8 +2,8 @@ const Product = require('../Products/Product');
 
 class MegaCoverage extends Product {
 
-    constructor(name, sellIn, price) {
-        super(name, sellIn, price)
+    constructor(pros) {
+        super(pros.name, pros.sellIn, pros.price)
     }
 
     update() {
